cff683303558ef14a5bb78751f7516c632bf0cba
sonjiseokk <thswltjr11@gmail.com>
2025-07-24 23:13:42 +0900

    init

---
+++ src/main/java/com/example/filefilter/FileFilterApplication.java
@@ -0,0 +1,13 @@
+package com.example.filefilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileFilterApplication.class, args);
+	}
+
+}
