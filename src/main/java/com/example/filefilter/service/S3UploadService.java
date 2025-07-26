896eac11de93d8b91c559ca619ee28f5fc1a66ac
sonjiseokk <thswltjr11@gmail.com>
2025-07-27 00:08:16 +0900

    feat: 브라우저가 열 수 있는 파일 attachment 설정

--- src/main/java/com/example/filefilter/service/S3UploadService.java
+++ src/main/java/com/example/filefilter/service/S3UploadService.java
@@ -1,27 +1,47 @@
 package com.example.filefilter.service;
 
-import io.awspring.cloud.s3.S3Template;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
-import java.util.UUID;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class S3UploadService {
 
-    private final S3Template s3Template;
+    private final S3Client s3Client;
 
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucket;
 
-    public void upload(MultipartFile file, String name, String extension) throws IOException {
-        String storedFileName = System.currentTimeMillis() + "_" + name + "." + extension;
+    public String upload(MultipartFile file, String name, String extension) throws IOException {
+        String storedFileName = System.currentTimeMillis() + "_" + name;
 
-        // 파일 업로드
-        s3Template.upload(bucket, storedFileName, file.getInputStream());
+        PutObjectRequest.Builder requestBuilder = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(storedFileName)
+                .contentType(file.getContentType());
+
+        if (isExecutableInBrowser(extension)) {
+            // 실행 불가능한 파일은 attachment로 다운로드 되도록 설정
+            requestBuilder.contentDisposition("attachment; filename=\"" + storedFileName + "\"");
+        }
+
+        PutObjectRequest putObjectRequest = requestBuilder.build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+        return storedFileName;
+    }
+
+
+    private boolean isExecutableInBrowser(String ext) {
+        return List.of("html", "htm", "svg", "js", "json", "xml", "css", "txt", "md")
+                .contains(ext.toLowerCase());
     }
 }
